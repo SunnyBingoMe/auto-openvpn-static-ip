0ad00380f52bc0681050a433865deaab65eabcd7
Sunny <Sunny>
2026-03-30 18:11:14 +0000

    minor

--- fix-route.sh
+++ fix-route.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root or with sudo." >&2
-  exit 1
-fi
 ip route del 172.22.0.0/16 2>/dev/null || true
 ip route del 172.23.0.0/16 2>/dev/null || true
 ip route replace 172.22.0.0/16 dev "$dev"
